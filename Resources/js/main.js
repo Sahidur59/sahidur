6c591f8afb20422566bdbff8bf9faa879e1c14b6
Sahidur Rahman <sksaik59@gmail.com>
2023-01-23 12:01:19 +0600

    first commit

---
+++ Resources/js/main.js
@@ -0,0 +1,11 @@
+//MIXITUP//
+var mixer = mixitup('.work-grid');
+
+//MENU//
+function openMenu(){
+    document.getElementById('menu-container').style.width ="100%"
+}
+
+function closeMenu(){
+    document.getElementById('menu-container').style.width ="0%"
+}
